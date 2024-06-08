99e3855a888809279dc2496bc7ff042b539c7465
rahul_thakur <rahulthakur.developer@gmail.com>
2024-06-08 21:07:28 +0530

    update the components of react

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom';
 
 export default function PrivateRoute() {
-  return (
-    <div>PrivateRoute</div>
-  )
+    const {currentUser} = useSelector((state)=>state.user);
+    return (
+      currentUser && Object.keys(currentUser).length ? <Outlet/> : <Navigate to = "/sign-in" />
+    )
 }
